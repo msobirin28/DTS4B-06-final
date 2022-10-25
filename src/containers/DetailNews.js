845e51948402b6b5e040a236e4435c029f7001cd
M. Sobirin <57650455+msobirin28@users.noreply.github.com>
2022-10-26 00:38:40 +0700

    fiture-favorite-v1

--- src/containers/DetailNews.js
+++ src/containers/DetailNews.js
@@ -5,7 +5,8 @@ import { useParams } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton, Menu, MenuItem } from "@mui/material";
 import thenewsapi from "../apis/thenewsapi";
 
 const HomeNews = () => {
@@ -13,6 +14,15 @@ const HomeNews = () => {
   const params = useParams();
   let date_1 = new Date(detail.published_at);
   let date_2 = new Date();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const days = (date_1, date_2) => {
     let difference = date_2.getTime() - date_1.getTime();
@@ -52,14 +62,55 @@ const HomeNews = () => {
       </Typography>
       <Box>
         <Card sx={{ maxWidth: "100%" }}>
-          <CardMedia
-            component="img"
-            image={`${detail.image_url}`}
-            alt={`${detail.title}`}
+          <Box
             sx={{
-              height: 400,
+              position: "relative",
+              textAlign: "center",
+              color: "white",
             }}
-          />
+          >
+            <CardMedia
+              component="img"
+              image={`${detail.image_url}`}
+              alt={`${detail.title}`}
+              sx={{
+                height: 400,
+              }}
+            />
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="open drawer"
+              onClick={handleMenu}
+              sx={{
+                mr: 1,
+                ml: 2,
+                position: "absolute",
+                top: 8,
+                right: 16,
+              }}
+            >
+              <MoreVertIcon color="primary" />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem>Simpan Favorite</MenuItem>
+            </Menu>
+          </Box>
+
           <CardContent>
             <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left" }}>
               {detail.title}
@@ -76,7 +127,7 @@ const HomeNews = () => {
               <Typography variant="body2">{detail.source}</Typography>
             </Box>
             <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", marginTop: 5, marginBottom: 5 }}>
-              {detail.description}
+              {detail.snippet ? detail.snippet : detail.description}
             </Typography>
             <Button variant="outlined" href={`${detail.url}`} target="_blank">
               Read More
